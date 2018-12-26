b6e5e8288d290abb4f2f5fd17dd355ea31f260c4
vivikey <civenan0001@163.com>
2018-12-26 18:30:51 +0800

    Udp 2018-12-26

---
+++ comm/Evaluate.js
@@ -0,0 +1,35 @@
+import $ from 'request.js'
+import regeneratorRuntime from 'regenerator-runtime.js'
+/**
+ * 评价
+ */
+let obj = {
+  PostForGoods() {
+    return '/api/addapi/goodsEvaluate'
+  },
+  ListForGoods() {
+    return '/api/showapi/showGoodsEvaluateList'
+  },
+  PostForStore() {
+		return '/api/addapi/storeEvaluate'
+  },
+  ListForStore() {
+		return '/api/showapi/showStoreEvaluateList'
+  },
+  async Do(data) {
+    return await new Promise((resolve, reject) => {
+      $.Post(this.url, data, r => {
+        resolve(r.data)
+      }, null, true)
+    })
+  }
+}
+
+const Evaluate = new Proxy(obj, {
+  get(target, property) {
+    target.url = target[property]()
+    return target.Do.bind(target)
+  }
+})
+
+export default Evaluate
